1501f54e35a9dda2da8377d28269f3cc68e8bb83
Drazen <drazeneduardo@gmail.com>
2021-10-13 15:47:47 -0300

    profile y skills add

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@ const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu");
 const closeMenuBtn = document.querySelector(".close-menu");
 
-function toogleMenu() {
-    menu.classList.toogle("menu_opened");
+function toggleMenu() {
+    menu.classList.toggle("menu_opened");
 }
-openMenuBtn.addEventListener("click", toogleMenu);
-closeMenuBtn.addEventListener("click", toogleMenu);
+openMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click", toggleMenu);
